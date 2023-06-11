7d0f3c96cd04971f57548a71c2abb42e8f0fc5fd
Igwe Becky <beckystar000@gmail.com>
2023-06-11 14:24:49 +0000

    Greetings

---
+++ Intro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Task given on 10th june
+#Personal project
+COMPANY="02TECH CONSULTING FIRM"
+TASK="CONTINUOUS INTEGRATION/CONTINUOUS DELIVERY TASK"
+NAME="IGWE BECKY"
+
+echo "Good day, My name is $NAME"
+echo "Prsentation on $TASK giving by $COMPANY,intern applicant"
+
